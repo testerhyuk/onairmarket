a29783d051f57ebef20ad1902ffeec64683294ad
Hyuk <testerhyuk214@gmail.com>
2024-12-07 17:01:23 +0900

    Initial commit

---
+++ src/main/java/com/market/onairmarket/OnairmarketApplication.java
@@ -0,0 +1,13 @@
+package com.market.onairmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnairmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnairmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/onairmarket/OnairmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.onairmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnairmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
